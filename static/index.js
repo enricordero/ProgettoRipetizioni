287d440915934cd7d73a0aee1935df01cc817591
Enrico Cordero <e.cordero.2645@vallauri.edu>
2025-04-11 19:10:53 +0200

    subject section finished, added all the images

--- static/index.js
+++ static/index.js
@@ -9,7 +9,8 @@ window.onload = function () {
 
     divCourses.find(".event_filter").find("a").on("click", function () {
         let indirizzo = ($(this).prop("id"))
-        getMateriePerIndirizzo(indirizzo)
+        let sigla = $(this).text()
+        getMateriePerIndirizzo(indirizzo, sigla)
     })
 
     async function getIndirizzi() {
@@ -21,7 +22,8 @@ window.onload = function () {
         }
     }
 
-    async function getMateriePerIndirizzo(indirizzo) {
+    async function getMateriePerIndirizzo(indirizzo, sigla) {
+        $(".event_box").empty()
         const request = await inviaRichiesta("GET", "/api/getMateriePerIndirizzo", { indirizzo })
         if (request) {
             for (const item of request.data) {
@@ -35,8 +37,46 @@ window.onload = function () {
                 }
             }
             materie.forEach(materia => {
-                console.log(materia)
+                const div = $(`
+                    <div class="col-lg-4 col-md-6 align-self-center mb-30 event_outer ${sigla.toLowerCase()}">
+                        <div class="events_item">
+                            <div class="thumb">
+                                <a href="#"><img class="imgMateria" src="assets/images/subjects/${materia.toLowerCase()}.jpg" alt=""></a>
+                                <span class="category">${materia}</span>
+                            </div>
+                            <div class="down-content">
+                                <span class="author">${indirizzo}</span>
+                                <h4>${materia}</h4>
+                            </div>
+                        </div>
+                    </div>
+                `);
+                div.hide()
+                div.appendTo(".event_box")
+                div.find("img").css({"filter": "brightness(50%)", "transition": "all 400ms"})
+                .on("mouseover",function(){
+                    $(this).css({"filter": "brightness(100%)"})
+                })
+                .on("mouseout",function(){
+                    $(this).css({"filter": "brightness(50%)", "transition": "all 400ms"})
+                });
+                div.show(400)
+                $(".event_box").css("height", "100%")
             });
         }
     }
 }
+/*
+<div1 class="col-lg-4 col-md-6 align-self-center mb-30 event_outer col-md-6 {{sigla.toLowerCase()}}">
+    <div2 class="events_item">
+        <div3 class="thumb">
+            <a href="#"><img src="assets/images/prova.jpg" alt=""></a>
+            <span class="category">prova</span>
+        </div3>
+        <div4 class="down-content">
+            <span class="author">nome_indirizzo</span>
+            <h4>{{materia}}</h4>
+        </div4>
+    </div2>
+</div1>
+*/
